63f1f4f3ebc1edb24f537b48c4a264dce2133fbd
Albert & Wiktor <79414658+albert-wiktor@users.noreply.github.com>
2021-05-20 01:33:27 +0200

    improvements

--- main.ts
+++ main.ts
@@ -1,32 +1,27 @@
 pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
     if (go) {
         start = control.millis()
-        music.playTone(262, music.beat(BeatFraction.Quarter))
     }
 })
 input.onButtonPressed(Button.A, function () {
-    basic.showIcon(IconNames.Chessboard)
+    images.arrowImage(ArrowNames.West).showImage(0)
     music.playMelody("C - C - C5 C5 C5 - ", 250)
     go = 1
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("" + (speed))
+    basic.showString("" + (game.score()))
 })
 pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
     if (go) {
         stop = control.millis()
-        music.playTone(523, music.beat(BeatFraction.Quarter))
-        speed = Math.round(30000 / (stop - start))
-        serial.writeValue("speed", speed)
-        basic.showString("" + (speed))
+        game.setScore(Math.round(30000 / (stop - start)))
+        basic.showString("" + (game.score()))
         go = 0
     }
 })
 let stop = 0
-let speed = 0
 let start = 0
 let go = 0
-DFRobotMaqueenPlus.I2CInit()
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 go = 0
