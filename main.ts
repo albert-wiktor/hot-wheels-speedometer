1f5ef50b9362a3539451c8a7ad22aa7391de34e5
Albert & Wiktor <79414658+albert-wiktor@users.noreply.github.com>
2021-05-21 23:31:52 +0200

    improvements

--- main.ts
+++ main.ts
@@ -4,19 +4,22 @@ pins.onPulsed(DigitalPin.P1, PulseValue.Low, function () {
     }
 })
 input.onButtonPressed(Button.A, function () {
-    images.arrowImage(ArrowNames.West).showImage(0)
+    images.arrowImage(ArrowNames.East).showImage(0)
     music.playMelody("C - C - C5 C5 C5 - ", 250)
     go = 1
 })
 input.onButtonPressed(Button.B, function () {
-    basic.showString("" + (game.score()))
+    basic.showNumber(game.score())
 })
 pins.onPulsed(DigitalPin.P2, PulseValue.Low, function () {
     if (go) {
         stop = control.millis()
-        game.setScore(Math.round(30000 / (stop - start)))
-        basic.showString("" + (game.score()))
+        game.setScore(Math.round(30 * 1000 / (stop - start)))
         go = 0
+        for (let index = 0; index < 5; index++) {
+            basic.clearScreen()
+            basic.showNumber(game.score())
+        }
     }
 })
 let stop = 0
@@ -25,3 +28,4 @@ let go = 0
 pins.setPull(DigitalPin.P1, PinPullMode.PullUp)
 pins.setPull(DigitalPin.P2, PinPullMode.PullUp)
 go = 0
+basic.showIcon(IconNames.Yes)
